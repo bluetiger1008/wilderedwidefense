fbf5bf98eab7179febfa4fea2a14888d7633d4bb
iam2611008 <dragonwarrior1008@yandex.com>
2018-01-26 01:01:26 -0800

    add category

--- wp-content/themes/baalspots/templates/content.php
+++ wp-content/themes/baalspots/templates/content.php
@@ -1,11 +1,27 @@
 <div class="column is-one-third" data-aos="fade-down" data-aos-anchor-placement="top-center">
 	<article <?php post_class(); ?>>
-		<div class="article-photo <?php echo has_post_thumbnail()? '': 'empty'; ?>" style="background-image: url('<?php echo has_post_thumbnail()? the_post_thumbnail_url( 'full' ) : null; ?>')">
+    <div class="article-photo">
+    	<figure class="image is-3by2">
+    		<?php if ( has_post_thumbnail() ) : ?>
+    			<img src="<?php the_post_thumbnail_url(); ?>"/>
+    		<?php endif; ?>
+    	</figure>
     </div>
     <div class="article-summary">
-      <p class="tags"><?php the_tags('',','); ?></p>
-      <div class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
-      <?php get_template_part('templates/entry-meta'); ?>
+    	<div class="article-title">
+	      <p class="categories">
+	      	<?php
+	      	$categories = get_the_category();
+					if ( ! empty( $categories ) ) {
+					    echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+					}
+					?>
+	      </p>
+	      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	    </div>
+      <div class="meta-data">
+	      <?php get_template_part('templates/entry-meta'); ?>
+	    </div>
     </div>
 	</article>
 </div>
