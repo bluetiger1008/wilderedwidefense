6f260f3c5d1cb5ecd90746a10155687036da756b
iam2611008 <dragonwarrior1008@yandex.com>
2018-01-29 10:37:36 -0800

    style fix and pinned article update

--- wp-content/themes/baalspots/templates/section-pinnedArticle.php
+++ wp-content/themes/baalspots/templates/section-pinnedArticle.php
@@ -1,30 +1,32 @@
 <section class="pinned-article">
-	<?php
-		$args = array( 'numberposts' => '1' );
-		$latest_post = wp_get_recent_posts( $args );
-		foreach( $latest_post as $latest ): ?>
-			<div class="columns">
-				<div class="column is-two-thirds article-image">
-					<figure class="image is-3by2">
-		    		<?php echo get_the_post_thumbnail( $latest->ID, 'thumbnail' ); ?>
-		    	</figure>
+	<?php $query = new WP_Query( array(
+      'post_type'=>'post',
+      'posts_per_page' => 1,
+  ) ); ?>
+  <?php if ( $query->have_posts() ) : ?>
+      <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+      	<div class="columns">
+					<div class="column is-two-thirds article-image">
+						<figure class="image is-3by2">
+			    		<img src="<?php the_post_thumbnail_url(); ?>"/>
+			    	</figure>
+					</div>
+					<div class="column">
+						<div class="content">
+							<p class="categories">
+			      	<?php
+			      	$categories = get_the_category($post->ID);
+							if ( ! empty( $categories ) ) {
+							    echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+							}
+							?>
+							</p>
+							<a href="<?php the_permalink(); ?>" class="post-title"><?php the_title(); ?></a>
+							<p class="post-content"><?php echo wp_trim_words( get_the_content(), 15, $more = '… ' ); ?></p>
+			      </div>
+					</div>
 				</div>
-				<div class="column">
-					<div class="content">
-						<p class="categories">
-		      	<?php
-		      	$categories = get_the_category($latest->ID);
-						if ( ! empty( $categories ) ) {
-						    echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
-						}
-						?>
-						</p>
-						<a href="<?php the_permalink(); ?>" class="post-title"><?php echo $latest["post_title"]?></a>
-						<p class="post-content"><?php echo wp_trim_words( $latest["post_content"], 30, $more = '… ' ); ?></p>
-		      </div>
-				</div>
-			</div>
-		<? endforeach; ?>
-	<? wp_reset_query(); ?>
-
+      <?php endwhile;
+          wp_reset_postdata(); ?>
+  <?php endif;?>
 </section>
