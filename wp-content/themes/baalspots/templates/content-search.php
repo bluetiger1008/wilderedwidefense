20b0b1e64141061d95622b228b832ebc45f925c2
iam2611008 <dragonwarrior1008@yandex.com>
2018-02-05 07:40:11 -0800

    minor fix

--- wp-content/themes/baalspots/templates/content-search.php
+++ wp-content/themes/baalspots/templates/content-search.php
@@ -1,7 +1,7 @@
 <article <?php post_class(); ?>>
   <header>
     <?php if(get_post_type() === 'post'): ?>
-      <p class="blog-mark">blog</p>
+      <p class="blog-mark">Blog</p>
     <?php endif; ?>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   </header>
