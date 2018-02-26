962d984f9058f4b3c144a9c1d7eee9300e676fc7
iam2611008 <dragonwarrior1008@yandex.com>
2018-02-26 00:22:03 -0800

    delete wp files

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<div class="<?php echo is_home() || is_category()? 'pg-blog' : '' ?>">
+	<div class="container">
+		<?php if (!have_posts()) : ?>
+		  <div class="alert alert-warning">
+		    <?php _e('Sorry, no results were found.', 'sage'); ?>
+		  </div>
+		  <?php get_search_form(); ?>
+		<?php endif; ?>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+		<?php if(!is_category()) {
+			get_template_part('templates/section', 'pinnedArticle');
+		}?>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+		<span class="yellow-border-bar"></span>
+
+		<div class="columns">
+			<?php while (have_posts()) : the_post(); ?>
+			  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+			<?php endwhile; ?>
+		</div>
+
+		<?php the_posts_navigation(); ?>
+	</div>
+
+	<?php get_template_part('templates/section', 'subscribe'); ?>
+</div>
